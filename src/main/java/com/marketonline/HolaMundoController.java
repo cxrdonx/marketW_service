035972bd0c3150846507c02d4b696180e4339390
José Cardona <69838225+cxrdonx@users.noreply.github.com>
2021-10-14 01:10:00 -0600

    Update HolaMundoController.java

--- src/main/java/com/marketonline/HolaMundoController.java
+++ src/main/java/com/marketonline/HolaMundoController.java
@@ -5,10 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/saludar")
 public class HolaMundoController {
-    @GetMapping("/hola")
-    public String saludar(){
-        return "holaputaf";
-    }
+
 }
